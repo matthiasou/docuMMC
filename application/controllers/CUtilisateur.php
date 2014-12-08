ea654adf1e354ebee78d87e1772e113d6450f090
maximeBnt <maxieevbm@gmail.com>
2014-12-08 11:55:40 +0100

    cool

--- application/controllers/CUtilisateur.php
+++ application/controllers/CUtilisateur.php
@@ -11,12 +11,13 @@ class CUtilisateur extends BaseCtrl{
     public function refresh($pb='') {
         if (!isset($_SESSION['user'])) {
             $this->load->view('VConnexion');
-            echo "Bienvenue " . $_SESSION['user']->getNom;
+            //echo "Bienvenue " . $this->session->userdata('user')->getNom();
+
             if ($pb == true)
                 echo 'Login ou password incorrect';
         }
         else{
-            echo "Bienvenue " . $_SESSION['user']->getNom;
+            echo "Bienvenue " . $this->session->userdata('user')->getNom();
         }
     }
 
@@ -25,14 +26,16 @@ class CUtilisateur extends BaseCtrl{
         $login = $_POST['username'];
         $password = $_POST['password'];
         $query = $this->doctrine->em->createQuery("SELECT u FROM Utilisateur u WHERE u.login='" . $login . "' AND u.password ='" . $password . "'");
-        if($utilisateur = $query->getResult()) {
+        if($utilisateur = $query->getSingleResult()) {
 
-            echo $utilisateur[0]->getLogin();
-            $_SESSION["user"] = $utilisateur[0];
+            //echo $utilisateur[0]->getLogin();
+            //$_SESSION["user"] = $utilisateur[0];
+            //$this->session->set_userdata('user', serialize($utilisateur));
+            //$this->session->userdata('user')->getNom();
 
-            //var_dump($utilisateur[0]);
+            var_dump($utilisateur);
             //var_dump($_SESSION["user"]);
-            echo $_SESSION["user"]->getGroupe()->getLibelle();
+            //echo $_SESSION["user"]->getGroupe()->getLibelle();
         }
         else{
             $pb = array('erreur de connexion');
