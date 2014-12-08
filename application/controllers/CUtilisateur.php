5bfc3f483a1935cd10455248bbb8a58bbfc1bd76
matthiaslecomte <matthiasou@yahoo.fr>
2014-12-08 20:16:59 +0100

    Lister tous les utilisateurs et pouvoir les supprimer

--- application/controllers/CUtilisateur.php
+++ application/controllers/CUtilisateur.php
@@ -8,6 +8,8 @@
 
 class CUtilisateur extends BaseCtrl{
 
+
+
     public function refresh($pb='') {
         if (!isset($_SESSION['user'])) {
             $this->load->view('VConnexion');
@@ -18,33 +20,88 @@ class CUtilisateur extends BaseCtrl{
         else{
             echo "Bienvenue " . $_SESSION['user']->getNom;
         }
+
     }
+
+
+
+
+
+    public function all(){
+        $this->load->library('jsUtils');
+        $this->jsutils->getAndBindTo(".delete", "click", "/docu/CUtilisateur/deleteUser", "body");
+        $this->jsutils->compile();
+        $query = $this->doctrine->em->createQuery("SELECT u FROM Utilisateur u");
+        $users = $query->getResult();
+        $this->load->view('VUtilisateur',array('utilisateurs'=>$users));
+
+    }
+
+    public function deleteUser($param){
+        $id=str_replace("utilisateur", "", $param[6]);
+       // var_dump($id);
+        $query = $this->doctrine->em->createQuery("SELECT u FROM Utilisateur u WHERE u.id=".$id);
+        $user = $query->getSingleResult();
+        //var_dump($user);
+        $this->doctrine->em->remove($user);
+        $this->doctrine->em->flush();
+        $this->all();
+
+
+    }
+
+
 
     public function connexion()
     {
         $login = $_POST['username'];
         $password = $_POST['password'];
         $query = $this->doctrine->em->createQuery("SELECT u FROM Utilisateur u WHERE u.login='" . $login . "' AND u.password ='" . $password . "'");
-        if($utilisateur = $query->getResult()) {
+        if($joueur = $query->getResult()) {
 
-            echo $utilisateur[0]->getLogin();
-            $_SESSION["user"] = $utilisateur[0];
-
-            //var_dump($utilisateur[0]);
-            //var_dump($_SESSION["user"]);
-            echo $_SESSION["user"]->getGroupe()->getLibelle();
+            echo $joueur[0]->getLogin();
+            var_dump($joueur[0]);
         }
         else{
             $pb = array('erreur de connexion');
             $this->refresh($pb);
         }
-    }
+        //echo $joueur->getLogin();
 
-    public function all(){
-        $query = $this->doctrine->em->createQuery("SELECT u FROM utilisateur u");
-        $users = $query->getResult();
-        $this->load->view('VUtilisateur',array('utilisateurs'=>$users));
-    }
 
+
+
+
+            /*$this->load->helper(array('form', 'url'));
+
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_rules('username', 'Nom d\'utilisateur', 'required');
+            $this->form_validation->set_rules('password', 'Mot de passe', 'required');
+            if ($this->form_validation->run() == FALSE)
+            {
+                $this->load->view('VIndex');
+            }
+            else
+            {
+                $this->load->view('VConnSuccess');
+            }*/
+
+            /*$query = $this->doctrine->em->createQuery("SELECT u FROM utilisateur u");
+            $users = $query->getResult();
+            $this->load->view('VIndex',array('utilisateurs'=>$users));
+
+            if ($joueur = DAO::getOne("Joueur", "login='" . $_POST["login"] . "' AND password= '" . $_POST["password"] . "'")) {
+                //var_dump($joueur);
+                $_SESSION["joueur1"] = $joueur;
+                //var_dump($joueur);
+            } else
+                echo 'Identifiants incorrects';
+
+            //echo JsUtils::doSomethingOn("#frmConnexion","hide");
+            //echo JsUtils::doSomethingOn("#inscription","hide");
+            echo JsUtils::get("CJoueur/index", "{}", "body");*/
+
+    }
 }
 ?>
--- application/views/VUtilisateur.php
+++ application/views/VUtilisateur.php
@@ -1,7 +1,9 @@
+<?php echo $library_src;?>
 <?php
 foreach ($utilisateurs as $user){
     echo ($user->getNom()."(".$user->getGroupe()->getLibelle().") <a class='delete' href='#' id =delete".$user->getId().">supprimer </a><br><br>");
 }
 ?>
+<?php echo $script_foot;?>
 
 
