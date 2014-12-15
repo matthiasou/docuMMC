deda709483ffc70e90e6a97956de42647695ca51
maximeBnt <paxman2@hotmail.fr>
2014-12-15 11:18:41 +0100

    afficher doc OK

--- application/views/VDocument.php
+++ application/views/VDocument.php
@@ -7,47 +7,37 @@
  */
 
 ?>
+<html ng-app>
+    <head>
+        <meta charset="UTF-8"/>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
+    </head>
 
-<table border="1" align="center">
-    <tr>
-        <th>
-            ID du document
-        </th>
-        <th>
-            Titre du document
-        </th>
-        <th>
-            Date de création du document
-        </th>
-    </tr>
-    <tr>
-        <td>
-            <?php
-            echo $doc[0]->getDocument()->getId();
-            ?>
-        </td>
-        <td>
-            <?php
-                echo $doc[0]->getDocument()->getTitre();
-            ?>
-        </td>
-        <td>
-            <?php
-            echo $doc[0]->getDocument()->getDateCreation()->format('Y-m-d H:i:s');
-            ?>
-        </td>
-    </tr>
+    <body>
+
+<p id="titDoc"><?php echo $doc[0]->getDocument()->getTitre();?></p>
+<p id="dateCrea"><?php echo $doc[0]->getDocument()->getDateCreation()->format('Y-m-d H:i:s');?></p>
+
+
+<div id="divSommaire" class="span6">
     <?php
-
+    //***** Sommaire affichant le titre de chaque parties les unes à la suite des autres
+    echo "<fieldset>";
+    echo "<legend>Sommaire : </legend>";
+        foreach ($doc as $docu) {
+            foreach ($docu->getParties() as $partie) {
+                echo "<span><a href='#divdoc". $partie->getNiveau() ."'>" . $partie->getTitre() . "</a>";
+            }
+        }
+    echo "</fieldset>";
     ?>
-</table>
+</div>
 
-<div id="divParties" class="divParties">
+
+<div id="divParties" class="span10">
     <?php
     //***** Affichage des parties les unes à la suite des autres
     foreach ($doc as $docu) {
-
-
         foreach ($docu->getParties() as $partie) {
             echo "<fieldset id='divdoc". $partie->getNiveau() ."' class='divdoc". $partie->getNiveau() ."'>";
             echo "<legend>" . $partie->getTitre() . "<input type='button' id='btnEdit' class='btnEdit' /></legend>";
@@ -57,3 +47,41 @@
     }
     ?>
 </div>
+
+<?php /*?>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
+    <input type="text" value="test" ng-model="query" placeholder="recherche">
+    <h1>Hello {{name}} !</h1>
+
+
+<?php var_dump($array);?>
+
+
+    <?php $array = null?>
+
+        <?php foreach($doc as $docu):?>
+            <?php foreach($docu->getParties() as $partie):?>
+
+                    <?php
+                    $array = $array + "{titre:'".$partie->getTitre()."', niveau:'".$partie->getNiveau()."'}";
+
+                    ?>
+            <?php endforeach;?>
+        <?php endforeach;?>
+
+<?php var_dump($array);?>
+    <div ng-init="users=[<?php echo $array;?>]">
+
+
+    <ul>
+        <li ng-repeat="user in users | filter:query">{{user}}
+
+        </li>
+    </ul>
+
+
+</div>
+*/?>
+    </body>
+</html>
+
