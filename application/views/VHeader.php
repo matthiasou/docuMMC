ba6841e499e4236ec324ca0f92cedbfee849ce11
maximeBnt <paxman2@hotmail.fr>
2014-12-16 14:44:46 +0100

    afficher doc OK

--- application/views/VHeader.php
+++ application/views/VHeader.php
@@ -9,7 +9,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="http://localhost/docu/assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
     <title>DocuMMC</title>
     <div class="head">
         <?php
