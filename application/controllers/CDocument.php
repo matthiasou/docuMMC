70035f0d727acd3fbadf6ce9d7ddc69bb4201e2a
maximeBnt <maxieevbm@gmail.com>
2014-12-18 12:03:24 +0100

    affiche docu ok

--- application/controllers/CDocument.php
+++ application/controllers/CDocument.php
@@ -21,10 +21,18 @@ class CDocument extends BaseCtrl {
 
     public function affichDoc($param){
         $doc = $this->getDoc($param);
-
         if ($doc != null){
+            foreach ($doc as $docu) {
+                foreach ($docu->getParties() as $partie) {
+                    $this->jsutils->click("#flecheSlide". $partie->getId() ."",$this->jsutils->slideToggle("#divParCon". $partie->getId()));
+                    echo $partie->getId();
+                }
+            }
+            $this->jsutils->click("#btnEdit",$this->jsutils->slideToggle("#divParties"));
+            $this->jsutils->compile();
             $this->load->view('VHeader');
             $this->load->view('VDocument', array('doc' => $doc));
+
         }
     }
 
--- application/views/VDocument.php
+++ application/views/VDocument.php
@@ -1,3 +1,6 @@
+
+<?php echo $library_src;?>
+<?php echo $script_foot;?>
 <?php
 /**
  * Created by PhpStorm.
@@ -10,14 +13,15 @@
 <html ng-app>
     <head>
         <meta charset="UTF-8"/>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
     </head>
 
     <body>
 
 <p id="titDoc"><?php echo $doc[0]->getDocument()->getTitre();?></p>
 <p id="dateCrea"><?php echo $doc[0]->getDocument()->getDateCreation()->format('Y-m-d H:i:s');?></p>
-
 
 <div id="divSommaire" class="span6">
     <ul>
@@ -42,16 +46,46 @@
     //***** Affichage des parties les unes à la suite des autres
     foreach ($doc as $docu) {
         foreach ($docu->getParties() as $partie) {
-            echo "<fieldset id='divdoc". $partie->getNiveau() ."' class='divdoc". $partie->getNiveau() ."'>";
-            echo "<legend>" . $partie->getTitre() . "<a href='#' id='btnEdit' class='btnEdit'><img src=' " . base_url() . "assets/images/icons/32x32/big46.png'></a></legend>";
-            echo "" . $partie->getContenu() . "";
+            echo "<fieldset id='divdoc". $partie->getId() ."' class='divdoc". $partie->getNiveau() ."'>";
+            echo "<legend><img id ='flecheSlide". $partie->getId() ."'class='flecheSlide' src=' " . base_url() . "assets/images/icons/arrow483.png'> " . $partie->getTitre() . "<a href='#' id='btnEdit". $partie->getId() ."' class='btnEdit'><img src=' " . base_url() . "assets/images/icons/32x32/big46.png'></a></legend>";
+            echo "<div id='divParCon". $partie->getId() ."'>" . $partie->getContenu() . "</div>";
             echo "</fieldset>";
         }
     }
     ?>
 </div>
 
-<?php /*?>
+
+<a href="#" class="back-to-top" id="back-to-top"><?php echo "<img src=' " . base_url() . "assets/images/icons/arrow.png'/>";?></a>
+<script src="jquery.js"></script>
+<script>
+    jQuery(document).ready(function() {
+        var offset = 220;
+        var duration = 100;
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > offset) {
+                jQuery('.back-to-top').fadeIn(duration);
+            } else {
+                jQuery('.back-to-top').fadeOut(duration);
+            }
+        });
+
+        jQuery('.back-to-top').click(function(event) {
+            event.preventDefault();
+            jQuery('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        })
+    });
+
+
+    $('.flecheSlide').click(function(){
+        $(this).addClass('rotated');
+    });
+
+
+</script>
+<?php
+/*?>
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.27/angular.min.js"></script>
     <input type="text" value="test" ng-model="query" placeholder="recherche">
     <h1>Hello {{name}} !</h1>
